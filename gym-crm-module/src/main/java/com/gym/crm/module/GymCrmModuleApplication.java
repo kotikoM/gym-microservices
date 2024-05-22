a8db8fd5c802570bfe4d85c2daf70aa9a8b6fce2
Konstantine Machavariani <kotiko2004@gmail.com>
2024-05-22 15:56:41 +0400

    added discovery server

--- gym-crm-module/src/main/java/com/gym/crm/module/GymCrmModuleApplication.java
+++ gym-crm-module/src/main/java/com/gym/crm/module/GymCrmModuleApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Gym CRM application", version = "1.0", description = "Simple customer service for gyms"))
+@EnableDiscoveryClient
 public class GymCrmModuleApplication {
 
     public static void main(String[] args) {
